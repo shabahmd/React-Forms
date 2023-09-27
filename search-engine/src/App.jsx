8af6d1c739b611aa23b725a4201d27e4654af11f
DoctorCnC <jaylim070@gmail.com>
2023-09-27 20:05:11 +0500

    second commit

--- search-engine/src/App.jsx
+++ search-engine/src/App.jsx
@@ -1,31 +1,69 @@
-import "./App.css";
-import JSONDATA from "./MOCK_DATA.json";
 import { useState } from "react";
+import "./App.css";
 
 function App() {
-    const [searchstate, setSearch] = useState("");
+    const [values, setValues] = useState({
+        firstName: "",
+        secondName: "",
+        email: "",
+    });
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setValues({ ...values, [name]: value });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (values.firstName && values.secondName && values.email) {
+            setSubmitted(true);
+        }
+    };
 
     return (
-        <div className="App">
-            <input
-                type="text"
-                placeholder="Search...."
-                onChange={(event) => setSearch(event.target.value)}
-            />
-
-            {JSONDATA.filter((val) => {
-                if (searchstate === "") {
-                    return true; // Include all items if searchstate is empty
-                } else if (val.first_name.toLowerCase().includes(searchstate.toLowerCase())) {
-                    return true; // Include the item if the first_name matches the searchstate
-                } else {
-                    return false; // Exclude the item if it doesn't match
-                }
-            }).map((val, key) => (
-                <div className="user" key={key}>
-                    <p>{val.first_name}</p>
-                </div>
-            ))}
+        <div className="form-container">
+            {submitted && (
+                <div className="success-message">Success! Thank you for registering</div>
+            )}
+            <form className="register-form" onSubmit={handleSubmit}>
+                <input
+                    onChange={handleInputChange}
+                    value={values.firstName}
+                    className="form-field"
+                    type="text"
+                    placeholder="First Name"
+                    name="firstName"
+                />
+                {submitted && !values.firstName && (
+                    <span className="error">Please enter a first name</span>
+                )}
+                <input
+                    onChange={handleInputChange}
+                    value={values.secondName}
+                    className="form-field"
+                    type="text"
+                    placeholder="Last Name"
+                    name="secondName"
+                />
+                {submitted && !values.secondName && (
+                    <span className="error">Please enter a last name</span>
+                )}
+                <input
+                    onChange={handleInputChange}
+                    value={values.email}
+                    className="form-field"
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                />
+                {submitted && !values.email && (
+                    <span className="error">Please enter an email address</span>
+                )}
+                <button className="form-field" type="submit">
+                    Register
+                </button>
+            </form>
         </div>
     );
 }
