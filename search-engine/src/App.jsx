200bded66fcefaddb5ba18410d4446be68cce207
DoctorCnC <jaylim070@gmail.com>
2023-09-28 23:52:33 +0500

    Initial commit

--- search-engine/src/App.jsx
+++ search-engine/src/App.jsx
@@ -1,71 +1,86 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
 
-function App() {
-    const [values, setValues] = useState({
-        firstName: "",
-        secondName: "",
-        email: "",
-    });
-    const [submitted, setSubmitted] = useState(false);
+export default function App() {
+    const questions = [
+        {
+            questionText: 'What is the capital of France?',
+            answerOptions: [
+                { answerText: 'New York', isCorrect: false },
+                { answerText: 'London', isCorrect: false },
+                { answerText: 'Paris', isCorrect: true },
+                { answerText: 'Dublin', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'Who is CEO of Tesla?',
+            answerOptions: [
+                { answerText: 'Jeff Bezos', isCorrect: false },
+                { answerText: 'Elon Musk', isCorrect: true },
+                { answerText: 'Bill Gates', isCorrect: false },
+                { answerText: 'Tony Stark', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'The iPhone was created by which company?',
+            answerOptions: [
+                { answerText: 'Apple', isCorrect: true },
+                { answerText: 'Intel', isCorrect: false },
+                { answerText: 'Amazon', isCorrect: false },
+                { answerText: 'Microsoft', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'How many Harry Potter books are there?',
+            answerOptions: [
+                { answerText: '1', isCorrect: false },
+                { answerText: '4', isCorrect: false },
+                { answerText: '6', isCorrect: false },
+                { answerText: '7', isCorrect: true },
+            ],
+        },
+    ];
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setValues({ ...values, [name]: value });
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (values.firstName && values.secondName && values.email) {
-            setSubmitted(true);
+    const handleSubmitbutton = (isCorrect) => {
+        if (isCorrect === "true") {
+            setScore(score + 1);
         }
-    };
+        const questionIncrement = currentQuestion + 1;
+        if (questionIncrement < questions.length) {
+            setCurrentQuestion(questionIncrement);
+
+        } else {
+            setShowScore(true)
+        }
+
+    }
+
 
     return (
-        <div className="form-container">
-            {submitted && (
-                <div className="success-message">Success! Thank you for registering</div>
+        <div className='app'>
+            {/* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+            {showScore ? (
+                <div className='score-section'>You scored {score} out of {questions.length}</div>
+            ) : (
+                <>
+                    <div className='question-section'>
+                        <div className='question-count'>
+                            <span>Question {currentQuestion + 1}</span>/{questions.length}
+                        </div>
+                        <div className='question-text'> {questions[currentQuestion].questionText}</div>
+                    </div>
+                    <div className='answer-section'>
+
+                        {questions[currentQuestion].answerOptions.map((value) => (
+                            <button onClick={() => handleSubmitbutton(value.isCorrect)} key={value.id}>{value.answerText}</button>
+                        ))}
+
+                    </div>
+                </>
             )}
-            <form className="register-form" onSubmit={handleSubmit}>
-                <input
-                    onChange={handleInputChange}
-                    value={values.firstName}
-                    className="form-field"
-                    type="text"
-                    placeholder="First Name"
-                    name="firstName"
-                />
-                {submitted && !values.firstName && (
-                    <span className="error">Please enter a first name</span>
-                )}
-                <input
-                    onChange={handleInputChange}
-                    value={values.secondName}
-                    className="form-field"
-                    type="text"
-                    placeholder="Last Name"
-                    name="secondName"
-                />
-                {submitted && !values.secondName && (
-                    <span className="error">Please enter a last name</span>
-                )}
-                <input
-                    onChange={handleInputChange}
-                    value={values.email}
-                    className="form-field"
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                />
-                {submitted && !values.email && (
-                    <span className="error">Please enter an email address</span>
-                )}
-                <button className="form-field" type="submit">
-                    Register
-                </button>
-            </form>
         </div>
     );
 }
-
-export default App;
